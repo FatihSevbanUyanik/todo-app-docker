90e4cdcb6fd1fccec439cb30d19ba7af25f28fa0
Fatih Sevban Uyanik <fatihsevban15@gmail.com>
2021-02-02 13:18:54 +0300

    update db config

--- backend/model/db.js
+++ backend/model/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const Keys = require('../keys')
 
-const sequelize = new Sequelize(Keys.PG_USER, Keys.PG_DATABASE, Keys.PG_PASSWORD, {
+const sequelize = new Sequelize(Keys.PG_DATABASE, Keys.PG_USER, Keys.PG_PASSWORD, {
     host: Keys.PG_HOST,
     dialect: Keys.PG_DIALECT
 })
